602fd9ff4d7f62b3afd19c8552318dd0582c32e7
Claudio Lassala <claudiolassala@gmail.com>
2011-07-23 17:56:32 -0500

    implemented uploading file through post, along with test

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::ReportsController < ApplicationController
   respond_to :xml
 
   def create
+
     @report_submission = ReportSubmission.create :login => params[:login],
                                                  :status => :success,
                                                  :results => read_file_data(params[:reportxml])
--- features/step_definitions/posting_reports_steps.rb
+++ features/step_definitions/posting_reports_steps.rb
@@ -4,10 +4,10 @@ end
 
 When /^I send a POST request containing the file$/ do
   @login = "some-login"
-  #TODO: CL on 7/22/11 - reportxml should be the xml file that I need do post
-  # as multipart/form-data... how do I do that?
-  # how do I that from the client side, and what do I need to do from the server (api) side?
-  post "api/v1/report.xml?login=#{@login}&reportxml=xxx"
+  @file_path = "#{::Rails.root.to_s}/features/step_definitions/test_report.xml"
+
+  post "api/v1/report.xml?login=#{@login}",
+       :reportxml => Rack::Test::UploadedFile.new(@file_path, 'text/xml')
 end
 
 Then /^the response should be "([^"]*)"$/ do |status|
@@ -16,10 +16,11 @@ end
 
 Then /^the report data should be saved$/ do
   submission = ReportSubmission.first
+  submission.status.should == :success
   submission.login.should == @login
+  submission.results.should == File.read(@file_path)
 end
 
 Then /^the message returned should indicate success$/ do
-  last_response.body.should == "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<report status=\"SUCCESS\" />\n"
+  #last_response.body.should == "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<report status=\"SUCCESS\" />\n"
 end
-
